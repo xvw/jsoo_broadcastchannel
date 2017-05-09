b7764b7583f40e5bfdbc20a6d2f657b2d2b073ee
xvw <xavier.vdw@gmail.com>
2017-05-09 11:59:37 +0200

    Append example

--- example/example.ml
+++ example/example.ml
@@ -5,8 +5,18 @@ module StringBus = BroadcastChannel.Make(
     type message = Js.js_string Js.t 
   end)
 
+module TestBus = BroadcastChannel.Make(
+  struct 
+    type message = <
+      x : int Js.readonly_prop; 
+      y : int Js.readonly_prop
+    > Js.t
+  end
+)
+
 
 let a = StringBus.create "test"
+let b = TestBus.create "test2"
 
 let () = Firebug.console##log(a)
 let () = Firebug.console##log(StringBus.name a)
@@ -23,6 +33,13 @@ let _ =
     (fun _ _ ->
       let _ = io "test" in
       let _ = StringBus.post a (Js.string "Hello World") in
+      let _ = TestBus.post 
+        b 
+        (object%js 
+          val x = 10 
+          val y = 12
+        end)
+      in 
       Lwt.return_unit 
     )
 
