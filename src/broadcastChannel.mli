0ce3eb5c4e5bc6098d8cfd022be60acc7a9f28b1
xvw <xavier.vdw@gmail.com>
2017-05-09 10:00:19 +0200

    Base of a documentation

--- src/broadcastChannel.mli
+++ src/broadcastChannel.mli
@@ -21,16 +21,42 @@
  * SOFTWARE.
  *)
 
+(** [jsoo_broadcastchannel] provides a wrapper around the BroadcastChannel API 
+    in JavaScript. 
+    The BroadcastChannel interface represents a named channel that any browsing context 
+    of a given origin can subscribe to. It allows communication between different documents 
+    (in different windows, tabs, frames or iframes) of the same origin. Messages are 
+    broadcasted via a message event fired at all BroadcastChannel objects listening 
+    to the channel.
+ *)
 
+
+(** {1 Types and classes} *)
+
+(** This class represent a broacasterChannel, it is a wrapper around the 
+    JavaScript implementation.
+ *)
 class type broadcaster = object 
   inherit Dom_html.eventTarget
   method name  : (Js.js_string Js.t) Js.readonly_prop
   method close : unit -> unit Js.meth
 end
 
+(** This type is the JavaScript part of a BroadcasterChannel. *)
 type t = broadcaster Js.t
 
+(** {1 API} *)
+
+(** Checks if the BroadcastChannel is available for the client *)
 val is_supported : unit -> bool
+
+(** Creates  a new BroadcastChannel (with a name). This function 
+    returns a [BroadcastChannel.t].
+ *)
 val create: string -> t
+
+(** Close a BroadcastChannel. *)
 val close: t -> unit
+
+(** Returns the name of a BroadcastChannel. *)
 val name: t-> string
